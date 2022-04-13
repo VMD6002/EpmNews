107c1fd5f0b6461b569e3134855c8425849655f0
VMD6002 <vmdchelembra@gmail.com>
2022-04-13 09:25:13 +0300

    initial

--- Firebase/firebase.js
+++ Firebase/firebase.js
@@ -2,13 +2,6 @@ import firebase from "firebase/compat/app";
 import "firebase/compat/firestore";
 
 const config = {
-  // apiKey: "AIzaSyC0bWgbwItqW3LAIr4Fyi9OSYPGyYMmhj0",
-  // authDomain: "epmnews1.firebaseapp.com",
-  // projectId: "epmnews1",
-  // storageBucket: "epmnews1.appspot.com",
-  // messagingSenderId: "12500658406",
-  // appId: "1:12500658406:web:3fc1977d770f7ecfd9f327",
-  // measurementId: "G-W6PL87VTPR",
   apiKey: process.env.API_KEY,
   authDomain: process.env.AUTH_DOMAIN,
   projectId: process.env.PROJECT_ID,
